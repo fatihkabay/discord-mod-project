73cd492fd5640c4aa1621f929f75f4be25698283
Fatih Kabay <mfthkabay@gmail.com>
2023-09-26 22:57:34 +0300

    initial commit

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-const {} = require('discord.js');
+import { Client } from "discord.js";
+
+const client = new Client({ intents: ["Guild", "GuildMessages"]});
+
+const TOKEN = 'MTE1NTU4NDU4MjU3ODYwMjEwNQ.GX4H-1._ZzVPhM5ggzD1tkd-30hRpk1tlityTv8qMyEdE';
+client.login(TOKEN);
