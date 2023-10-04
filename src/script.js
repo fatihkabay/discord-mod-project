14a7a4e354300358398d53044fec6a9274dfbe59
Fatih Kabay <mfthkabay@gmail.com>
2023-10-04 21:43:52 +0300

    the bot is online

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const { Client, IntentsBitField } = require('discord.js');
+import { Client, IntentsBitField} from 'discord.js';
 
 const client = new Client({
     intents: [
@@ -8,3 +8,9 @@ const client = new Client({
         IntentsBitField.Flags.MessageContent,
     ]
 })
+
+client.on('ready', (e) => {
+   console.log(`✅${e.user.tag} is online!`)
+})
+
+client.login("MTE1NTU4NDU4MjU3ODYwMjEwNQ.G39ybC.f3tOAv7aUJydUcpBhxKsHS-aWkRwmVs3N3K2R0")
